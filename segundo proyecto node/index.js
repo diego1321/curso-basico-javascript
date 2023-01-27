7a5ea5e6d489b6952eaf45b8b5558967de70a54f
diego <diegoalejandrop574@gmail.com>
2023-01-27 16:31:42 -0300

    primer y segundo proyecto

---
+++ segundo proyecto node/index.js
@@ -0,0 +1,11 @@
+const personal = {
+    nombre: "Diego",
+    edad : 19,
+    desarrollador : false,
+    nacimiento : new Date("december 03 2003"),
+    libro_favorito:{
+        titulo:"mitos, monstruos y caos en la mitologia griega",
+        autor: "James Davies",
+        fecha:2021
+    }
+};
